045c7f518a0a4a81251ac4308980a565b91715b3
Vladimir <vladimir_semashko@bk.ru>
2022-03-26 20:27:37 +0300

    Add project files.

---
+++ MyBlog.Service/IPageRepository.cs
@@ -0,0 +1,20 @@
+﻿using MyBlog.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Service
+{
+  public  interface IPageRepository
+    {
+        IEnumerable<Page> LastPage { get; }
+        void Save(Page page);
+        IEnumerable<Page> PageIEnum { get; }
+        List<Page> PageList { get; }
+        Page Details(int? Id);
+       
+    }
+}
